d75e58c5e4557dfed01f12dc784c26cc725c9c30
Hailey <haileyckeen@gmail.com>
2020-07-21 12:38:54 -0400

    Created basic scaffolding for search form

--- src/App.js
+++ src/App.js
@@ -2,16 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert(`A gem was submitted: ${this.state.value}`);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           Search for Ruby Gems
         </div>
-        <input
-          type="text"
-          placeholder="Search"
-        />
+        <div>
+          <form onSubmit={this.handleSubmit}>
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
     );
   }
